4c9e79e4373957b86b2396eb54b55046418765fb
liuqing <liuqing@vt.edu>
2016-12-01 14:36:10 -0500

    change map colors with zoom and pan

--- statestweets.php
+++ statestweets.php
@@ -1,16 +1,35 @@
 <?php
-$from_date = date_parse(base64_decode($_GET['f']));
-$to_date = date_parse(base64_decode($_GET['t']));
+$from_date = date("Y-m-d H:i:s", strtotime(base64_decode($_GET['f'])));
+$to_date = date("Y-m-d H:i:s", strtotime(base64_decode($_GET['t'])));
 
-echo "state,count";
-echo "\nMA,";
-echo mt_rand(0,10);
-echo "\nVA,";
-echo mt_rand(0,10);
-echo "\nNJ,";
-echo mt_rand(0,10);
-echo "\nNY,";
-echo mt_rand(0,10);
-echo "\nCA,";
-echo mt_rand(0,10);
+require("config.php");
+$link = mysqli_init();
+$success = mysqli_real_connect(
+    $link, 
+    $host, 
+    $user, 
+    $password, 
+    $db,
+    $port
+);
+
+$query = "SELECT location as state, COUNT(*) as count FROM tweet_vis 
+	WHERE created_time >= '$from_date' AND created_time <= '$to_date'
+	GROUP BY location";
+
+$result = mysqli_query($link, $query);
+
+$data = array();
+while($row = $result->fetch_assoc())
+{
+    $data[] = $row;
+}
+echo json_encode($data);  
+
+/* free result set */
+mysqli_free_result($result);
+
+/* close connection */
+mysqli_close($link);
+
 ?>
